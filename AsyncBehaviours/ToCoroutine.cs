c510b2402b357418b74c2b51545c2233dd911694
Alex <alex.steed88@gmail.com>
2022-04-15 17:10:50 +0900

    Added an As Coroutine Extension method for tasks.

---
+++ AsyncBehaviours/ToCoroutine.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+
+namespace com.longtailgames.asyncbehaviours
+{
+    public static class AsyncAsCoroutine
+    {
+        public static IEnumerator AsCoroutine(this Task task)
+        {
+            while (!task.IsCompleted)
+            {
+                yield return null;
+            }
+
+            if (task.IsFaulted)
+            {
+                ExceptionDispatchInfo.Capture(task.Exception).Throw();
+            }
+
+            yield return null;
+        }
+
+        public static IEnumerator AsCoroutine<T>(this Task<T> task)
+        {
+            while (!task.IsCompleted)
+            {
+                yield return null;
+            }
+
+            if (task.IsFaulted)
+            {
+                ExceptionDispatchInfo.Capture(task.Exception).Throw();
+            }
+
+            yield return null;
+        }
+    }
+}
